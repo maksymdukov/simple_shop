13e91eefa591da1e0e21bcd2231085f67e967478
Maksym Dukov <maxopowerful@gmail.com>
2019-07-27 19:08:07 +0300

    Updates

---
+++ precache-manifest.817dfbbfc51a9e79ad0ef6b27be397f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b45a6f479b65de5179b3",
+    "url": "/simple_shop/static/css/main.f7a0be61.chunk.css"
+  },
+  {
+    "revision": "b45a6f479b65de5179b3",
+    "url": "/simple_shop/static/js/main.e8f6d261.chunk.js"
+  },
+  {
+    "revision": "bd029302596b4ee1cf58",
+    "url": "/simple_shop/static/js/runtime~main.f553c328.js"
+  },
+  {
+    "revision": "96b071cc1b5834cf48bb",
+    "url": "/simple_shop/static/css/2.bc25b3c8.chunk.css"
+  },
+  {
+    "revision": "96b071cc1b5834cf48bb",
+    "url": "/simple_shop/static/js/2.5128e0eb.chunk.js"
+  },
+  {
+    "revision": "ace42bcbbb1fb7ddf32e",
+    "url": "/simple_shop/static/js/3.7a941c99.chunk.js"
+  },
+  {
+    "revision": "dba22297bbaa2159900f",
+    "url": "/simple_shop/static/js/4.ef9d69ea.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/simple_shop/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "169bf5d6149b3fd5e5cfe5624d072b0e",
+    "url": "/simple_shop/static/media/TheBurger_video_cover_03.169bf5d6.mp4"
+  },
+  {
+    "revision": "7e423e589a5a893d5a3d44e8caa84c5f",
+    "url": "/simple_shop/static/media/o-nas.7e423e58.png"
+  },
+  {
+    "revision": "4635ac31e4b441a808b475f82d794340",
+    "url": "/simple_shop/static/media/onas-2.4635ac31.jpg"
+  },
+  {
+    "revision": "b7a64930a5b951309800376d1094057e",
+    "url": "/simple_shop/static/media/o-nas-3.b7a64930.jpg"
+  },
+  {
+    "revision": "c8d74525b69184448dffef0e6390eec9",
+    "url": "/simple_shop/static/media/Person-01.c8d74525.svg"
+  },
+  {
+    "revision": "2ade5fe7404c460a2489074b68930985",
+    "url": "/simple_shop/static/media/notification.2ade5fe7.mp3"
+  },
+  {
+    "revision": "5235c23b4fe34ea20da0837cb315a588",
+    "url": "/simple_shop/index.html"
+  }
+];
